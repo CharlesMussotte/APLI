48da57d3ec0e392b44393895c1a825ee6729efa2
Charlatan92 <xcharlatan92@gmail.com>
2022-06-15 16:30:37 +0200

    Teste la méthode MemeVille

---
+++ junit/MembresTestMembreMemeVille.java
@@ -0,0 +1,21 @@
+-import org.junit.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class MembresTestMembreMemeVille {
+
+    @org.junit.jupiter.api.Test
+
+    void membreMemeVille() {
+        
+
+        assertEquals(False,Itineraire.ajouterUneVille())// Teste lorsque la méthode est appelée vide
+
+        assertEquals(True,Itineraire.ajouterUneVille(villeDeTest))// Teste une ville à ajouter
+
+// IsMapContaining
+    }
+}
