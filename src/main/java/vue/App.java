068955e9b27595e866225c29ac53c86af38cddfa
theob <1infotbuvsq@gmail.com>
2022-06-16 17:27:34 +0200

    passage sous javaFX

---
+++ src/main/java/vue/App.java
@@ -0,0 +1,12 @@
+package vue;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.File;
+
+public class App {
+
+
+}
