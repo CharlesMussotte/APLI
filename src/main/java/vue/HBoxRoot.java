569d4386deeeb9771d3e3060cf667190c4cae34a
theob <1infotbuvsq@gmail.com>
2022-06-16 17:45:58 +0200

    Création de HBoxRoot.java

---
+++ src/main/java/vue/HBoxRoot.java
@@ -0,0 +1,9 @@
+package vue;
+
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.HBox;
+
+public class HBoxRoot extends HBox {
+    private MenuBar menuBar;
+    private GridPaneAffichage affichage;
+}
