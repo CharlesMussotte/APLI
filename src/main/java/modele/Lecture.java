3778075ac68726f4ebaa29fda560b94403a8584e
theob <1infotbuvsq@gmail.com>
2022-06-16 22:55:53 +0200

    Création de la class Lecture et de la fonction pour lire un scenario

---
+++ src/main/java/modele/Lecture.java
@@ -0,0 +1,26 @@
+package modele;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Lecture {
+    public static Scenario lectureScenario (File fichier) throws IOException {
+        Scenario scenario = new Scenario();
+        BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
+        String ligne ;
+        StringTokenizer tokenizer ;
+        do {
+            ligne = bufferEntree.readLine ();
+            if (ligne != null) {
+                tokenizer = new StringTokenizer(ligne ," ->");
+            }
+        }
+        while (ligne != null );
+        bufferEntree.close();
+        return scenario;
+    }
+
+}
