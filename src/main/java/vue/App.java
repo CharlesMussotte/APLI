388b0d29c22f17f83e47e39630926b8cdf9572aa
theob <1infotbuvsq@gmail.com>
2022-06-16 17:29:27 +0200

    Ecriture de la base de l'application

--- src/main/java/vue/App.java
+++ src/main/java/vue/App.java
@@ -6,7 +6,17 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.File;
 
-public class App {
-
-
+public class App extends Application{
+    public void start(Stage stage)   {
+        File fileCss = new File("css"+File.separator+"fond.css");
+        HBox root = new HBoxRoot();
+        Scene scene = new Scene(root,600,380);
+        scene.getStylesheets().add(fileCss.toURI().toString());
+        stage.setScene(scene);
+        stage.setTitle("Calendrier du mois");
+        stage.show();
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
