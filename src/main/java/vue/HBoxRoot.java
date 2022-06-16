8f00bc0e3daddff7583e692d0b22486c9bdb53c0
theob <1infotbuvsq@gmail.com>
2022-06-16 22:31:34 +0200

    Création des methodes get

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -1,20 +1,21 @@
 package vue;
 
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
 public class HBoxRoot extends HBox {
     private MenuBar menuBar;
     private GridPaneAffichage affichage;
-    public HBoxRoot(){
-        menuBar= new MenuBar();
+    public HBoxRoot() {
+        menuBar = new MenuBar();
         Menu senario = new Menu("Senario");
-        MenuItem presenario= new MenuItem("Senario 1");
-        MenuItem secsenario= new MenuItem("Senario 2");
-        senario.getItems().addAll(presenario,secsenario);
         menuBar.getMenus().addAll(senario);
         this.getChildren().addAll(menuBar);
+    }
+    public MenuBar getMenuBar() {
+        return menuBar;
+    }
+    public GridPaneAffichage getAffichage() {
+        return affichage;
     }
 }
