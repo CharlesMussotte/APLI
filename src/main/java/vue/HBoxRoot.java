b9c09c85eeb0650f99fdfd56993ae47876dd6f9a
theob <1infotbuvsq@gmail.com>
2022-06-16 21:46:46 +0200

    Mise en place de la barre de menu dans APLI

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -1,9 +1,20 @@
 package vue;
 
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
 
 public class HBoxRoot extends HBox {
     private MenuBar menuBar;
     private GridPaneAffichage affichage;
+    public HBoxRoot(){
+        menuBar= new MenuBar();
+        Menu senario = new Menu("Senario");
+        MenuItem presenario= new MenuItem("Senario 1");
+        MenuItem secsenario= new MenuItem("Senario 2");
+        senario.getItems().addAll(presenario,secsenario);
+        menuBar.getMenus().addAll(senario);
+        this.getChildren().addAll(menuBar);
+    }
 }
