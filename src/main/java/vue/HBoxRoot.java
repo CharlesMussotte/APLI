a028ece3accc5eea03fd6db5a9aa457a4edf3088
theob <1infotbuvsq@gmail.com>
2022-06-17 10:38:06 +0200

    Mise à jour de HBoxRoot

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -4,22 +4,10 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
 public class HBoxRoot extends HBox {
-    private MenuBar menuBar;
     private GridPaneAffichage affichage;
     public HBoxRoot() {
-        menuBar = new MenuBar();
-        Menu senario = new Menu("Senario");
-        ToggleGroup senar = new ToggleGroup();
-        for (int i = 0; i < 4; i++) {
-            RadioMenuItem presenario = new RadioMenuItem("Senario " + i);
-            senario.getItems().addAll(presenario);
-            presenario.setToggleGroup(senar);
-        }
-        menuBar.getMenus().addAll(senario);
-        this.getChildren().addAll(menuBar);
-    }
-    public MenuBar getMenuBar() {
-        return menuBar;
+        affichage =new GridPaneAffichage();
+        this.getChildren().addAll(affichage);
     }
     public GridPaneAffichage getAffichage() {
         return affichage;
