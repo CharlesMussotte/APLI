104745c83d18cd1ca67e901b325131ec97d4861e
theob <1infotbuvsq@gmail.com>
2022-06-17 16:06:45 +0200

    Ajout du controleur à la HBox

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -1,15 +1,22 @@
 package vue;
 
+import controleur.Controleur;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
 public class HBoxRoot extends HBox {
     private GridPaneAffichage affichage;
+    private static Controleur controleur;
     public HBoxRoot() {
         affichage =new GridPaneAffichage();
+        controleur = new Controleur();
         this.getChildren().addAll(affichage);
     }
     public GridPaneAffichage getAffichage() {
         return affichage;
+    }
+
+    public static Controleur getControleur() {
+        return controleur;
     }
 }
