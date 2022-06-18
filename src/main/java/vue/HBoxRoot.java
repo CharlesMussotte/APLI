88d09b4aeb9e07c69121c112fefc3e4b58bac583
theob <1infotbuvsq@gmail.com>
2022-06-18 18:21:01 +0200

    Ajout de la GridPane pour les données à HBox

--- src/main/java/vue/HBoxRoot.java
+++ src/main/java/vue/HBoxRoot.java
@@ -3,17 +3,30 @@ package vue;
 import controleur.Controleur;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 
 public class HBoxRoot extends HBox {
     private static GridPaneAffichage affichage;
+    private static GridPaneDonnees donnees;
     private static Controleur controleur;
+    private StackPane stackPane;
     public HBoxRoot() {
         affichage =new GridPaneAffichage();
+        donnees = new GridPaneDonnees();
         controleur = new Controleur();
-        this.getChildren().addAll(affichage);
+        stackPane = new StackPane();
+        affichage.setId("opaque");
+        donnees.setId("opaque");
+        stackPane.getChildren().add(donnees);
+        stackPane.getChildren().add(affichage);
+        this.getChildren().add(stackPane);
     }
     public static GridPaneAffichage getAffichage() {
         return affichage;
+    }
+
+    public static GridPaneDonnees getDonnees() {
+        return donnees;
     }
 
     public static Controleur getControleur() {
