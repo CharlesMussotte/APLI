a31874ffa3a37ad88c6bf697353ca0f7e2a73aef
theob <1infotbuvsq@gmail.com>
2022-06-18 23:45:39 +0200

    Mis à jour du controleur (suppretion)

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -1,30 +1,20 @@
 package controleur;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.RadioMenuItem;
-import modele.Lecture;
-import modele.Scenario;
-import vue.App;
 import vue.GridPaneAffichage;
+import vue.GridPaneDonnees;
 import vue.HBoxRoot;
-
-import java.io.File;
-import java.io.IOException;
 
 public class Controleur implements EventHandler {
     public void handle(Event event) {
         GridPaneAffichage affichage = HBoxRoot.getAffichage();
+        GridPaneDonnees donnes =HBoxRoot.getDonnees();
 
         if (event.getSource() instanceof RadioMenuItem) {
-            try {
-                Scenario senario = Lecture.lectureScenario(new File("Scenario"+File.separator+"scenario_0.txt"));
-                affichage.updateScenario(senario);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            String nom =((RadioMenuItem) event.getSource()).getText();
-            affichage.getMenu().setText(nom);
+            int numScene = (int)((RadioMenuItem) event.getSource()).getUserData();
+            donnes.MajDonnees(numScene);
         }
-
     }
 }
