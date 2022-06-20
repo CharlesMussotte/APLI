ed98af499ce8392262a072c6d52fbc232b10b8ca
Charlatan92 <xcharlatan92@gmail.com>
2022-06-20 23:00:34 +0200

    Commentaires Controleur

--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -1,3 +1,5 @@
+//----- IMPORTS -----
+
 package controleur;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -8,32 +10,39 @@ import modele.Scenario;
 import vue.GridPaneAffichage;
 import vue.GridPaneDonnees;
 import vue.HBoxRoot;
-
 import java.io.File;
 import java.io.IOException;
 
+
+//----- CONTROLEUR -----
+
 public class Controleur implements EventHandler {
+
     public void handle(Event event) {
+
         GridPaneAffichage affichage = HBoxRoot.getAffichage();
-        GridPaneDonnees donnes =HBoxRoot.getDonnees();
-        if (event.getSource() instanceof Button) {
+        GridPaneDonnees donnes = HBoxRoot.getDonnees();
+
+        if (event.getSource() instanceof Button) { // Passage de l'affichage aux données
             HBoxRoot.changement();
         }
 
         if (event.getSource() instanceof RadioMenuItem) {
-            int numScene = (int)((RadioMenuItem) event.getSource()).getUserData();
+            int numScene = (int)((RadioMenuItem) event.getSource()).getUserData(); // Recupére les valeurs du scénario choisi
+            
+            // CHOIX DU SCENARIO
 
-            if (numScene == 0){
+            if (numScene == 0){ // Scénario 0
                 try {
                     Scenario senario = Lecture.lectureScenario(new File("TourAPLI\\Scenar\\scenario_0.txt"));
                     donnes.majDonnees(senario);
-                    affichage.updateScenario(senario);
+                    affichage.updateScenario(senario); // MAJ des acheteurs/vendeurs
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    throw new RuntimeException(e); // Si le fichier est inexistant
                 }
 
             }
-            if (numScene == 1){
+            if (numScene == 1){ // Scénario 1
                 try {
                     Scenario senario = Lecture.lectureScenario(new File("TourAPLI\\Scenar\\scenario_1_1.txt"));
                     donnes.majDonnees(senario);
@@ -43,7 +52,7 @@ public class Controleur implements EventHandler {
                 }
 
             }
-            if (numScene == 2){
+            if (numScene == 2){ // Scénario 2
                 try {
                     Scenario senario = Lecture.lectureScenario(new File("TourAPLI\\Scenar\\scenario_1_2.txt"));
                     donnes.majDonnees(senario);
@@ -53,7 +62,7 @@ public class Controleur implements EventHandler {
                 }
 
             }
-            if (numScene == 3){
+            if (numScene == 3){ // Scénario 3
                 try {
                     Scenario senario = Lecture.lectureScenario(new File("TourAPLI\\Scenar\\scenario_2_1.txt"));
                     donnes.majDonnees(senario);
@@ -63,7 +72,7 @@ public class Controleur implements EventHandler {
                 }
 
             }
-            if (numScene == 04){
+            if (numScene == 04){ // Scénario 4
                 try {
                     Scenario senario = Lecture.lectureScenario(new File("TourAPLI\\Scenar\\scenario_2_2.txt"));
                     donnes.majDonnees(senario);
