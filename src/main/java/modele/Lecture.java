c2cc789d41ab992d33fd725452986fb83cd1ee70
theob <1infotbuvsq@gmail.com>
2022-06-20 21:11:19 +0200

    Création de la méthode lectureVille

--- src/main/java/modele/Lecture.java
+++ src/main/java/modele/Lecture.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Lecture {
@@ -46,22 +47,32 @@ public class Lecture {
         return membres;
     }
 
-    public static Ville lectureVilleDistance (File fichier) throws IOException {
-        Ville ville = new Ville();
+    public static ArrayList lectureVille (File fichier) throws IOException {
+        ArrayList<ArrayList> distanceEtVille = new ArrayList();
+        ArrayList<String> villes = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> touteDistance = new ArrayList<>();
         BufferedReader bufferEntree = new BufferedReader(new FileReader(fichier));
         String ligne ;
         StringTokenizer tokenizer ;
         do {
             ligne = bufferEntree.readLine ();
             if (ligne != null) {
-                tokenizer = new StringTokenizer(ligne ,"-");
-                membres.ajoutMembre(tokenizer.nextToken(), tokenizer.nextToken());
+                tokenizer = new StringTokenizer(ligne ," ");
+                villes.add(tokenizer.nextToken());
+                ArrayList<Integer> distance = new ArrayList<>();
+                for (int i=0; i<29; i++){
+                    distance.add(Integer.valueOf(tokenizer.nextToken()));
+                }
+                touteDistance.add(distance);
             }
         }
         while (ligne != null );
         bufferEntree.close();
-        return membres;
+        distanceEtVille.add(villes);
+        distanceEtVille.add(touteDistance);
+        return distanceEtVille;
     }
+
 
 
 
