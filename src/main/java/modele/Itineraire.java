0a8e86c8fc9f1009b8d8a70ed0985958e9ac4d0a
theob <1infotbuvsq@gmail.com>
2022-06-20 16:21:57 +0200

    In progress

--- src/main/java/modele/Itineraire.java
+++ src/main/java/modele/Itineraire.java
@@ -25,6 +25,8 @@ public class Itineraire {
         ArrayList<String> acheteurs = (ArrayList<String>) scenario.getAcheteurs();
         ArrayList<String> villeVendres = new ArrayList();
         ArrayList<String> villeAcheteurs = new ArrayList();
+
+
         for (String vendeur : vendres){
             int nb =0;
             while (!vendeur.equals(noms.get(nb))){
@@ -42,34 +44,55 @@ public class Itineraire {
         for (int i=0; i<villeVendres.size();i++){
             if(treeSuccesseurs.containsKey(villeVendres.get(i))){
                 treeSuccesseurs.get(villeVendres.get(i)).add(villeAcheteurs.get(i));
-                int ii = treeNBPredesseurs.get(villeAcheteurs.get(i));
-                ii+=1;
-                treeNBPredesseurs.put(villeAcheteurs.get(i),ii);
+                if (treeNBPredesseurs.get(villeAcheteurs.get(i)) == null){
+                    treeNBPredesseurs.put(villeAcheteurs.get(i),1);
+                }
+                else{
+                    int ii = treeNBPredesseurs.get(villeAcheteurs.get(i));
+                    ii+=1;
+                    treeNBPredesseurs.put(villeAcheteurs.get(i),ii);
+                }
             }
             else{
                 ArrayList<String> arrayListVendeur = new ArrayList<>();
                 ArrayList<String> arrayList =new ArrayList<>();
-                arrayListVendeur.add(villes.get(i));
+
+                arrayListVendeur.add(villeAcheteurs.get(i));
                 treeSuccesseurs.put(villeVendres.get(i),arrayListVendeur);
-                treeSuccesseurs.put(villeVendres.get(i),arrayList);
                 treeNBPredesseurs.put(villeVendres.get(i),1);
-                treeNBPredesseurs.put(villeAcheteurs.get(i),1);
+                if (treeNBPredesseurs.get(villeAcheteurs.get(i)) == null){
+                    treeNBPredesseurs.put(villeAcheteurs.get(i),1);
+                    treeSuccesseurs.put(villeAcheteurs.get(i),arrayList);
+                }
+                else {
+                    int ii = treeNBPredesseurs.get(villeAcheteurs.get(i));
+                    ii+=1;
+                    treeNBPredesseurs.put(villeAcheteurs.get(i),ii);
+                }
             }
         }
         ArrayList<String> list = new ArrayList<>();
         for (String str : villeVendres) {
-            list.add(str);
+            if (treeNBPredesseurs.get(str)== 1){
+                list.add(str);
+            }
 
         }
         treeSuccesseurs.put("Velizy",list);
         treeNBPredesseurs.put("Velizy",0);
         sources.add("Velizy");
-        while (sources.size()>0){
+
+        System.out.println(treeSuccesseurs);
+        while (sources != null){
 
             for (String ville : treeNBPredesseurs.keySet()) {
-                Integer i = treeNBPredesseurs.get(ville);
-                i -=1;
-                treeNBPredesseurs.put(ville,i);
+
+                if (treeSuccesseurs.get(sources.get(0)).contains(ville) && treeNBPredesseurs.get(ville) != 0){
+                    Integer i = treeNBPredesseurs.get(ville);
+                    i -=1;
+                    treeNBPredesseurs.put(ville,i);
+                    System.out.println(treeNBPredesseurs);
+                }
             }
             listeVilleItineraire.add(sources.get(0));
             sources.remove(0);
